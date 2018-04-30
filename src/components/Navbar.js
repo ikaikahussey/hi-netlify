68c24efbb67cd5fee24a5af9c7d4dd73e7c213b5
Ikaika Hussey <ikaikahussey@gmail.com>
2018-04-30 01:41:07 -1000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,6 @@ const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
